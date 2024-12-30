4f5c99fb4ee12ddd1b52904369df4c1ed7a5be91
Mohamedattiadev <mohamedattia.dev@gmail.com>
2024-12-30 03:12:42 +0300

    12/30    3:12

--- SentebiolabCodes/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {  
-    jwtSecret: "fokingsecretkey",
-    jwtSession: {
-        session: false
-    },
-		session: false
-};
--- none/newSente/Frontend/.vite/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-DLU427U7.js";
-import "./chunk-UTEJFLXC.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=react_jsx-runtime.js.map
