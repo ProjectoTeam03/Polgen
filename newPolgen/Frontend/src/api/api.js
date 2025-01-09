8fd3ce422e757903898f3a7fff2ac19535dd8697
root <root@srv637318.hstgr.cloud>
2025-01-09 03:48:38 +0000

    Api with server backend

--- newPolgen/Frontend/src/api/api.js
+++ newPolgen/Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Your API base URL
+  baseURL: "http://93.127.162.95:5000/api", // Your API base URL
   headers: {
     "Content-Type": "application/json",
   },
